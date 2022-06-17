ba6d02aeb7e51c9be4e89df27b3dfe271c2cd8ef
muktiag29 <muktiag290998@gmail.com>
2022-06-17 19:19:41 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// DIKUMPULKAN HARI MINGGU TERAKHIR JAM 12 MALAM
+
+/**
+ * Perang melawan Alien, buatlah program untuk memfilter kategori penduduk yang berhak dan wajib perang melawan alien.
+ */
+
+/**
+ * Tingkatan Jabatan untuk pria
+ * Tentara > Officer I > Officer II > Jendral Perang
+ */
+
+/**
+ * Tingkatan Jabatan untuk wanita
+ * Medis > Officer Medis
+ */
+
+/**
+ * Medis >>> Usia minimal 18 tahun, Maksimal 30 tahun.
+ * Officer Medis >>> Usia minimal 30 tahun, Maksimal 55 tahun, Pengalaman militer 10 tahun.
+ * Tentara >>> Usia minimal 18 tahun, Maksimal 30 tahun, Pengalaman militer 5 tahun.
+ * Officer I >>> Usia minimal 25 tahun, Maksimal 45 tahun, Pengalaman militer 7 tahun.
+ * Officer II >>> Usia minimal 30 tahun, Maksimal 50 tahun, Pengalaman militer 9 tahun.
+ * Jendral Perang >>> Usia minimal 40 tahun, Maksimal 65 tahun, Pengalaman militer 15 tahun.
+ */
+
+/**
+ * Baru >>> Pengalaman militer dibawah 5 tahun.
+ * Berpengalaman >>> Pengalaman militer diatas 5 tahun, dibawah 10 tahun.
+ * Sangat Berpengalaman >>> Pengalaman militer diatas 10 tahu
+ */
+
+let nama; // String
+let umur; // Number
+let jenisKelamin; // String
+let pengalamanMiliterTahun; // Number
+let pengalamanMiliterKategori; // String
+let jabatan; // String
+
+/**
+ * Jawaban: Ubah value dari variable [jabatan] menjadi => "HALO [nama] DENGAN UMUR [umur] DAN JENIS KELAMIN [jenisKelamin] DENGAN PENGALAMAN MILITER ANDA YANG [pengalamanMiliterKategori] ANDA BERHAK MENDUDUKI [jabatan]"
+ */
+
+// your code here
